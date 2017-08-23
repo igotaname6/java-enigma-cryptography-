fb743d15b8338ff3448952506e2e9f4455d40ccd
Szymon Wojtas <szymon.wojtas@gmail.com>
2017-08-23 15:08:11 +0200

    add key requirment checking to initialaze

--- src/app/TerminalTranslator.java
+++ src/app/TerminalTranslator.java
@@ -1,23 +1,28 @@
 package app;
 
+import java.io.Scanner;
 
 
 public class TerminalTranslator implements Module{
     private inEncipherMode;
     private cipherName;
     private service;
+    private key;
 
 
-
-    public TerminalTranslator(Boolean inEncipherMode, String cipherName){
+    public TerminalTranslator(Boolean inEncipherMode, String cipherName, String key){
         this.isEncipherMode = inEncipherMode;
         this.cipherName = cipherName;
         tihs.service = null;
+        this.key = key;
     }
 
     public void initialize(ServiceProvider provider){
         EnigmaService service;
         this.service = provider.getByName(cipherName);
+        if (this.service.isKeyRequired()){
+            this.service.setKey(this.key)
+        }
     }
     public String getName(){
         if (this.service == null){
