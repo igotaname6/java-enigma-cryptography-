d8483931ca65c49cd663b87e64225b2c7cd9e37e
Szymon Wojtas <szymon.wojtas@gmail.com>
2017-08-23 13:18:09 +0200

    add initialize method to TerminalTranslator class

--- src/app/TerminalTranslator.java
+++ src/app/TerminalTranslator.java
@@ -5,11 +5,18 @@ package app;
 public class TerminalTranslator implements Module{
     private inEncipherMode;
     private cipherName;
+    private service;
+
 
 
     public TerminalTranslator(Boolean inEncipherMode, String cipherName){
         this.isEncipherMode = inEncipherMode;
         this.cipherName = cipherName;
+        tihs.service = null;
     }
 
+    public void initialize(ServiceProvider provider){
+        EnigmaService service;
+        this.service = provider.getByName(cipherName);
+    }
 }
