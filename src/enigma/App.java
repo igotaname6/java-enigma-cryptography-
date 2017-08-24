36d2e3f62c34a1ecff5a980e6fbd515558a403e6
Fesh <maciej.ukowski@gmail.com>
2017-08-24 16:12:50 +0200

    Add TerminalArgsValidator, TerminalArgsCipherValidator methods

--- src/enigma/App.java
+++ src/enigma/App.java
@@ -1,13 +1,36 @@
 package enigma;
 
+import registry.ServiceRepository;
 import services.EnigmaService;
+import services.Rot13Enigma;
+import services.AtbashCipher;
 import app.Module;
+import app.TerminalTranslator;
+import java.util.ArrayList;
 
 public class App {
 
-	public static void main(String... args){
-
-
-
+	public static void main(String[] args) {
+		
 	}
+
+	public static void TerminalArgsValidator(String[] args) throws Exception {
+		int argsLength;
+
+		argsLength = args.length;
+		if (argsLength < 1) {
+			throw new Exception("Wrong input");
+		}
+	}
+
+	public static void TerminalArgsCipherValidator(String[] args) throws Exception {
+		int argsLength;
+
+		argsLength = args.length;
+		if (argsLength < 2) {
+			throw new Exception("Enter cipher name");
+		}
+	}
+	
 }
+
