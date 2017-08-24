a7ba82829add5d14e659e5b43309e2cba86b8cb9
Szymon Wojtas <szymon.wojtas@gmail.com>
2017-08-24 11:21:39 +0200

    add constructor to VinegreCipher class

--- src/services/vigenereCipher.java
+++ src/services/vigenereCipher.java
@@ -4,4 +4,9 @@ public class VingenereCipher implements EnigmaService{
     private static String name = "Vinegere Cipher"
     private static Boolean isKeyRequired = true;
 
+    private String key;
+
+    public VingenereCipher(){
+        this.key = null;
+    }
 }
